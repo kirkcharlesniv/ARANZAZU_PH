aadaec1dba8aa305b91b4e62eaf9f4458d3df1b2
Kirk Charles Niverba <KPNiverba@aranzazushrine.ph>
2019-02-23 19:43:43 +0800

    Added Fragments for News, Events, Bible, Resources, Menu Added Animations for Fragment Switching (Fade In, Fade Out)

--- app/src/main/java/ph/aranzazushrine/aranzazuph/HomeActivity.java
+++ app/src/main/java/ph/aranzazushrine/aranzazuph/HomeActivity.java
@@ -2,15 +2,23 @@ package ph.aranzazushrine.aranzazuph;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     FirebaseUser currentUser;
     BottomNavigationView bottomNavigationView;
+    NewsFragment newsFragment = new NewsFragment();
+    EventsFragment eventsFragment = new EventsFragment();
+    BibleFragment bibleFragment = new BibleFragment();
+    ResourcesFragment resourcesFragment = new ResourcesFragment();
+    MenuFragment menuFragment = new MenuFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +30,36 @@ public class HomeActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+
         bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.setItemIconTintList(null);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.menu_news);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.menu_news:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.frameContainer, newsFragment).commit();
+                return true;
+
+            case R.id.menu_events:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.frameContainer, eventsFragment).commit();
+                return true;
+
+            case R.id.menu_bible:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.frameContainer, bibleFragment).commit();
+                return true;
+
+            case R.id.menu_resources:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.frameContainer, resourcesFragment).commit();
+                return true;
+
+            case R.id.menu_menu:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.frameContainer, menuFragment).commit();
+                return true;
+        }
+        return false;
+    }
 }
